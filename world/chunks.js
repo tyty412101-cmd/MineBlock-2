5982e4a4537d25a1ce9a212f44a2db294bfaaa6b
tyty412101-cmd <tyty412101@gmail.com>
2026-04-20 08:16:49 -0400

    Update chunks.js

--- world/chunks.js
+++ world/chunks.js
@@ -1,68 +1,15 @@
-// world/chunks.js
-
 const CHUNK_SIZE = 16;
-const WORLD_HEIGHT = 64;
 
-const chunks = {};
+function getBlock(world, x, y) {
+    if (y < 0 || y >= world.length) return BLOCK.AIR;
+    if (x < 0 || x >= world[0].length) return BLOCK.AIR;
 
-function getChunkKey(cx, cz) {
-  return `${cx},${cz}`;
+    return world[y][x];
 }
 
-export function generateChunk(cx, cz) {
-  const chunk = [];
+function setBlock(world, x, y, type) {
+    if (y < 0 || y >= world.length) return;
+    if (x < 0 || x >= world[0].length) return;
 
-  for (let x = 0; x < CHUNK_SIZE; x++) {
-    chunk[x] = [];
-
-    for (let z = 0; z < CHUNK_SIZE; z++) {
-      chunk[x][z] = [];
-
-      for (let y = 0; y < WORLD_HEIGHT; y++) {
-        // Terrain height
-        const height = 10 + Math.floor(Math.sin((cx * CHUNK_SIZE + x) * 0.2) * 3);
-
-        if (y > height) chunk[x][z][y] = "air";
-        else if (y === height) chunk[x][z][y] = "grass";
-        else if (y > height - 3) chunk[x][z][y] = "dirt";
-        else chunk[x][z][y] = "stone";
-      }
-    }
-  }
-
-  return chunk;
-}
-
-export function getChunk(cx, cz) {
-  const key = getChunkKey(cx, cz);
-
-  if (!chunks[key]) {
-    chunks[key] = generateChunk(cx, cz);
-  }
-
-  return chunks[key];
-}
-
-export function getBlock(x, y, z) {
-  const cx = Math.floor(x / CHUNK_SIZE);
-  const cz = Math.floor(z / CHUNK_SIZE);
-
-  const chunk = getChunk(cx, cz);
-
-  const lx = ((x % CHUNK_SIZE) + CHUNK_SIZE) % CHUNK_SIZE;
-  const lz = ((z % CHUNK_SIZE) + CHUNK_SIZE) % CHUNK_SIZE;
-
-  return chunk[lx][lz][y] || "air";
-}
-
-export function setBlock(x, y, z, type) {
-  const cx = Math.floor(x / CHUNK_SIZE);
-  const cz = Math.floor(z / CHUNK_SIZE);
-
-  const chunk = getChunk(cx, cz);
-
-  const lx = ((x % CHUNK_SIZE) + CHUNK_SIZE) % CHUNK_SIZE;
-  const lz = ((z % CHUNK_SIZE) + CHUNK_SIZE) % CHUNK_SIZE;
-
-  chunk[lx][lz][y] = type;
+    world[y][x] = type;
 }
