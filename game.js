c4fa830cee70c16abf9fd100d84c24033cdc9279
tyty412101-cmd <tyty412101@gmail.com>
2026-04-20 08:15:50 -0400

    Update game.js

--- game.js
+++ game.js
@@ -7,6 +7,15 @@ const ctx = canvas.getContext("2d");
 const world = createWorld(800, 120);
 const player = createPlayer(10, 10);
 
+let lastTime = 0;
+let fps = 0;
+
+initDebug();
+
+// starter items
+inventory.slots[0] = "grass";
+inventory.slots[1] = "stone";
+
 function update() {
     handleInput(player);
     applyPhysics(player);
@@ -14,6 +23,20 @@ function update() {
 
 function renderFrame() {
     render(ctx, world, player);
+
+    renderInventory();
+    updateDebug(player, fps);
 }
 
-startGameLoop(update, renderFrame);
+function gameLoop(time) {
+    const delta = time - lastTime;
+    fps = 1000 / (delta || 1);
+    lastTime = time;
+
+    update();
+    renderFrame();
+
+    requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
