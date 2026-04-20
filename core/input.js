0e802079c3b301714ba59c6b07343925069a99f3
tyty412101-cmd <tyty412101@gmail.com>
2026-04-20 07:58:02 -0400

    Create input.js

---
+++ core/input.js
@@ -0,0 +1,9 @@
+const keys = {};
+
+window.addEventListener("keydown", e => {
+    keys[e.key.toLowerCase()] = true;
+});
+
+window.addEventListener("keyup", e => {
+    keys[e.key.toLowerCase()] = false;
+});
