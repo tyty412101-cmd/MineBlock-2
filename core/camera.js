541f240fc0c8f750f97d86a79e562d6275273fd5
tyty412101-cmd <tyty412101@gmail.com>
2026-04-20 08:08:48 -0400

    Update camera.js

--- core/camera.js
+++ core/camera.js
@@ -1,6 +1,6 @@
 function getCamera(player) {
     return {
-        x: player.x * 32 - window.innerWidth / 2,
-        y: player.y * 32 - window.innerHeight / 2
+        x: player.x * 32 - canvas.width / 2,
+        y: player.y * 32 - canvas.height / 2
     };
 }
