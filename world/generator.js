2a1536227d76445b1d681faffb110d39d77db513
tyty412101-cmd <tyty412101@gmail.com>
2026-04-20 08:06:43 -0400

    Update generator.js

--- world/generator.js
+++ world/generator.js
@@ -5,10 +5,14 @@ function generateWorld(width, height) {
         const row = [];
 
         for (let x = 0; x < width; x++) {
-            if (y > 35) row.push(BLOCK_TYPES.STONE);
-            else if (y > 30) row.push(BLOCK_TYPES.DIRT);
-            else if (y === 30) row.push(BLOCK_TYPES.GRASS);
-            else row.push(BLOCK_TYPES.AIR);
+
+            let block = BLOCK.AIR;
+
+            if (y === 50) block = BLOCK.GRASS;
+            else if (y > 50 && y <= 55) block = BLOCK.DIRT;
+            else if (y > 55) block = BLOCK.STONE;
+
+            row.push(block);
         }
 
         world.push(row);
