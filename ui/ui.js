27a7ba114b47819ab5ae577b60b225254e6f1163
tyty412101-cmd <tyty412101@gmail.com>
2026-04-20 08:09:45 -0400

    Update ui.js

--- ui/ui.js
+++ ui/ui.js
@@ -1,1 +1,8 @@
-// UI placeholder
+function renderUI(ctx, player) {
+    ctx.fillStyle = "white";
+    ctx.font = "16px monospace";
+
+    ctx.fillText("Mineblock", 10, 20);
+    ctx.fillText("X: " + Math.floor(player.x), 10, 40);
+    ctx.fillText("Y: " + Math.floor(player.y), 10, 60);
+}
